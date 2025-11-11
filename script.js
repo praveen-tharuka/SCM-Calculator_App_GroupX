96795d16975dc8eb8d5ed28b79c876065bbe1ca2
Praveen Tharuka <praveentharuka1234@gmail.com>
2025-11-11 10:52:45 +0530

    InitialSetupDone

---
+++ script.js
@@ -0,0 +1,33 @@
+// Calculator functions will be added by team members
+
+// Helper function to get input values
+function getInputs() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    return { num1, num2 };
+}
+
+// Helper function to display result
+function displayResult(value) {
+    document.getElementById('result').textContent = value;
+}
+
+// Addition function - TO BE IMPLEMENTED BY STUDENT 2
+function add() {
+    alert('Addition not implemented yet!');
+}
+
+// Subtraction function - TO BE IMPLEMENTED BY STUDENT 3
+function subtract() {
+    alert('Subtraction not implemented yet!');
+}
+
+// Multiplication function - TO BE IMPLEMENTED BY STUDENT 4
+function multiply() {
+    alert('Multiplication not implemented yet!');
+}
+
+// Division function - TO BE IMPLEMENTED BY STUDENT 5
+function divide() {
+    alert('Division not implemented yet!');
+}
